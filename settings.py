ea074e8ac735884865f4d5d3e4fa673ad092be4e
Amir Khan <amir.khan.sch@gmail.com>
2023-03-30 06:32:37 +0100

    this is my first commit for the game

---
+++ settings.py
@@ -0,0 +1,39 @@
+# Options / Settings
+TITLE = "SKYWARD!"
+WIDTH = 550
+HEIGHT = 700
+FPS = 60
+FONT_NAME = 'arial'
+HIGHSCORE = "highscore.txt"
+SPRITESHEET = "spritesheet_jumper.png"
+
+
+# Player properties
+PLAYER_ACC = 0.7
+PLAYER_FRICTION = -0.12
+PLAYER_GRAV = 0.8
+PLAYER_JUMP = 23
+
+# Game properties
+BOOST_POWER = 60
+POW_SPAWN_PCT = 10
+
+# Starting platforms
+PLATFORM_LIST = [(0, HEIGHT - 40,),
+                 (WIDTH / 2 - 50, HEIGHT * 3 / 4 - 50),
+                 (125, HEIGHT - 350),
+                 (350, 200),
+                 (175, 100)]
+
+# define colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+LIGHTBLUE = (0, 155, 155)
+lightpurple = (194, 150, 218)
+BGCOLOR = BLACK
+
+
